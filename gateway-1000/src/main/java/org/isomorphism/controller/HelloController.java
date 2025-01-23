b0f0c9fee0bffa6fe6108673ef4fa572813ca773
Isomorphism <jw6699@nyu.edu>
2025-01-23 10:52:55 -0500

    integrated redis with the project

--- gateway-1000/src/main/java/org/isomorphism/controller/HelloController.java
+++ gateway-1000/src/main/java/org/isomorphism/controller/HelloController.java
@@ -1,18 +1,35 @@
 package org.isomorphism.controller;
 
+import jakarta.annotation.Resource;
+import org.isomorphism.base.BaseInfoProperties;
+import org.isomorphism.utils.RedisOperator;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("g")
-public class HelloController {
+public class HelloController extends BaseInfoProperties {
+
+//    @Resource
+//    private RedisOperator redis;
 
     // 127.0.0.1:1000/g/hello
 
     @GetMapping("hello")
     public Object hello() {
         return "Hello World";
+    }
+
+    @GetMapping("setRedis")
+    public Object setRedis(String k, String v) {
+        redis.set(k, v);
+        return "setRedis OK~";
+    }
+
+    @GetMapping("getRedis")
+    public Object getRedis(String k) {
+        return redis.get(k);
     }
 
 }
