a086bc4de8b0bb09a82311a3265f8c38bc9d2897
Isomorphism <jw6699@nyu.edu>
2025-01-23 23:44:05 -0500

    added a IPLimitFilter class for anti exploiting

---
+++ gateway-1000/src/main/java/org/isomorphism/filter/IPLimitFilter.java
@@ -0,0 +1,33 @@
+package org.isomorphism.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.isomorphism.base.BaseInfoProperties;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class IPLimitFilter extends BaseInfoProperties implements GlobalFilter, Ordered {
+    /**
+     * 需求：
+     * 判断某个请求的ip在20秒内的请求次数是否超过3次
+     * 如果超过3次，则限制访问30秒
+     * 等待30秒静默后，才能够继续恢复访问
+     */
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        // 默认放行请求到后续的路由（服务）
+        return chain.filter(exchange);
+    }
+
+    // 过滤器的顺序，数字越小则优先级越大
+    @Override
+    public int getOrder() {
+        return 1;
+    }
+
+}
