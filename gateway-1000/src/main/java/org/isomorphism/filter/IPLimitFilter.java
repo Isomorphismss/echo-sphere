c05619b523b87c1e360ba71fe38cb7199a5df7a4
Isomorphism <jw6699@nyu.edu>
2025-01-24 00:08:45 -0500

    added error message return wrapper function

--- gateway-1000/src/main/java/org/isomorphism/filter/IPLimitFilter.java
+++ gateway-1000/src/main/java/org/isomorphism/filter/IPLimitFilter.java
@@ -1,13 +1,22 @@
 package org.isomorphism.filter;
 
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.isomorphism.base.BaseInfoProperties;
+import org.isomorphism.grace.result.GraceJSONResult;
+import org.isomorphism.grace.result.ResponseStatusEnum;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 @Slf4j
@@ -20,8 +29,42 @@ public class IPLimitFilter extends BaseInfoProperties implements GlobalFilter, O
      */
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        log.info("IPLimitFilter 当前的执行顺序order为1");
+        if (1 == 1) {
+            // 终止请求，返回错误信息
+           return  renderErrorMsg(exchange, ResponseStatusEnum.SYSTEM_ERROR_BLACK_IP);
+        }
         // 默认放行请求到后续的路由（服务）
         return chain.filter(exchange);
+    }
+
+    // 重新包装并且返回错误信息
+    public Mono<Void> renderErrorMsg(ServerWebExchange exchange,
+                                    ResponseStatusEnum statusEnum) {
+        // 1. 获得响应response
+        ServerHttpResponse response = exchange.getResponse();
+
+        // 2. 构建jsonResult
+        GraceJSONResult jsonResult = GraceJSONResult.exception(statusEnum);
+
+        // 3. 设置header类型
+        if (!response.getHeaders().containsKey("Content-Type")) {
+            response.getHeaders().add("Content-Type",
+                    MimeTypeUtils.APPLICATION_JSON_VALUE);
+        }
+
+        // 4. 修改当前response的状态码code为500
+        response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        // 5. 转换json并且向response中写入数据
+        String resultJson = new Gson().toJson(jsonResult);
+
+        DataBuffer buffer = response
+                .bufferFactory()
+                .wrap(resultJson.getBytes(StandardCharsets.UTF_8));
+
+        return response.writeWith(Mono.just(buffer));
     }
 
     // 过滤器的顺序，数字越小则优先级越大
