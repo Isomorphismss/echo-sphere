2db35717a172f184987cd6970f84e854d0af57ef
Isomorphism <jw6699@nyu.edu>
2025-01-24 21:15:49 -0500

    integrated message service to the project

--- api/auth-service-88/src/main/java/org/isomorphism/controller/HelloController.java
+++ api/auth-service-88/src/main/java/org/isomorphism/controller/HelloController.java
@@ -1,5 +1,7 @@
 package org.isomorphism.controller;
 
+import jakarta.annotation.Resource;
+import org.isomorphism.utils.SMSUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +12,19 @@ public class HelloController {
 
     // 127.0.0.1:88/a/hello
 
+    @Resource
+    private SMSUtils smsUtils;
+
     @GetMapping("hello")
     public Object hello() {
         return "Hello World";
+    }
+
+    @GetMapping("sms")
+    public Object sms() throws Exception {
+        smsUtils.sendSMS("12345678901", "9875");
+
+        return "Send SMS OK~";
     }
 
 }
