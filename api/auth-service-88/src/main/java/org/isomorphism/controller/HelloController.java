7eddbee7a73abfb050debb892048d0f972f4a50f
Isomorphism <jw6699@nyu.edu>
2025-01-24 21:34:55 -0500

    added a async task class to send sms

--- api/auth-service-88/src/main/java/org/isomorphism/controller/HelloController.java
+++ api/auth-service-88/src/main/java/org/isomorphism/controller/HelloController.java
@@ -1,6 +1,7 @@
 package org.isomorphism.controller;
 
 import jakarta.annotation.Resource;
+import org.isomorphism.SMSTask;
 import org.isomorphism.utils.SMSUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,9 @@ public class HelloController {
     @Resource
     private SMSUtils smsUtils;
 
+    @Resource
+    private SMSTask smsTask;
+
     @GetMapping("hello")
     public Object hello() {
         return "Hello World";
@@ -25,6 +29,13 @@ public class HelloController {
         smsUtils.sendSMS("12345678901", "9875");
 
         return "Send SMS OK~";
+    }
+
+    @GetMapping("smsTask")
+    public Object smsTask() throws Exception {
+        smsTask.sendSMSInTask("12312312312", "8111");
+
+        return "Send SMS In Task OK~";
     }
 
 }
