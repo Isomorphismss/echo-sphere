2ec1c0185166d30759ad3e911d8143390aeaf605
Isomorphism <jw6699@nyu.edu>
2025-01-29 00:43:03 -0500

    added mapper and user service classes

---
+++ api/auth-service-88/src/main/java/org/isomorphism/service/UsersService.java
@@ -0,0 +1,15 @@
+package org.isomorphism.service;
+
+/**
+ * <p>
+ * 用户表 服务类
+ * </p>
+ *
+ * @author isomorphism
+ * @since 2025-01-29
+ */
+public interface UsersService {
+
+
+
+}
---
+++ api/auth-service-88/src/main/java/org/isomorphism/service/impl/UsersServiceImpl.java
@@ -0,0 +1,20 @@
+package org.isomorphism.service.impl;
+
+import org.isomorphism.base.BaseInfoProperties;
+import org.isomorphism.service.UsersService;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 用户表 服务实现类
+ * </p>
+ *
+ * @author isomorphism
+ * @since 2025-01-29
+ */
+@Service
+public class UsersServiceImpl extends BaseInfoProperties implements UsersService {
+
+
+
+}
