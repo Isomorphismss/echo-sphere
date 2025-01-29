8fff35bea90cb09200c173ee6b82cd439b83b467
Isomorphism <jw6699@nyu.edu>
2025-01-28 23:43:41 -0500

    added a register function to handle registration

--- api/auth-service-88/src/main/java/org/isomorphism/controller/PassportController.java
+++ api/auth-service-88/src/main/java/org/isomorphism/controller/PassportController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.apache.commons.lang3.StringUtils;
 import org.isomorphism.base.BaseInfoProperties;
 import org.isomorphism.grace.result.GraceJSONResult;
+import org.isomorphism.pojo.bo.RegisterLoginBO;
 import org.isomorphism.tasks.SMSTask;
 import org.isomorphism.utils.IPUtil;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,6 +39,24 @@ public class PassportController extends BaseInfoProperties {
 
         // 把验证码存入到redis中，用于后续注册/登录的校验
         redis.set(MOBILE_SMSCODE + ":" + mobile, code, 30 * 60);
+
+        return GraceJSONResult.ok();
+    }
+
+    @PostMapping("register")
+    public GraceJSONResult register(RegisterLoginBO registerLoginBO,
+                                    HttpServletRequest request) throws Exception {
+        String mobile = registerLoginBO.getMobile();
+        String code = registerLoginBO.getSmsCode();
+
+        // 1. 从redis中获得验证码进行校验判断是否匹配
+
+        // 2. 根据mobile查询数据库，如果用户存在，则提示不能重复注册
+        // 2.1 如果查询数据库中用户为空，则表示用户没有注册过，则需要进行用户信息数据的入库
+
+        // 3. 用户注册成功后，删除redis中的短信验证码使其失效
+
+        // 4. 返回用户数据给前端
 
         return GraceJSONResult.ok();
     }
