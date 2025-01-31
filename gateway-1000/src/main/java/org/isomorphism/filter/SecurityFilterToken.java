94e17bced00ab1d887233b0423a46cf8ebc86960
Isomorphism <jw6699@nyu.edu>
2025-01-31 15:17:20 -0500

    get token and verify in gateway to intercept or pass

--- gateway-1000/src/main/java/org/isomorphism/filter/SecurityFilterToken.java
+++ gateway-1000/src/main/java/org/isomorphism/filter/SecurityFilterToken.java
@@ -2,10 +2,13 @@ package org.isomorphism.filter;
 
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.isomorphism.base.BaseInfoProperties;
+import org.isomorphism.grace.result.ResponseStatusEnum;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.server.ServerWebExchange;
@@ -46,8 +49,24 @@ public class SecurityFilterToken extends BaseInfoProperties implements GlobalFil
         // 4. 代码到达此处，表示请求被拦截，需要进行校验
         log.info("当前请求的路径[{}]被拦截...", url);
 
-        // 默认放行请求到后续的路由（服务）
-        return chain.filter(exchange);
+        // 5. 从header中获得用户的id以及token
+        HttpHeaders headers = exchange.getRequest().getHeaders();
+        String userId = headers.getFirst(HEADER_USER_ID);
+        String userToken = headers.getFirst(HEADER_USER_TOKEN);
+        log.info("userId = {}", userId);
+        log.info("userToken = {}", userToken);
+
+        // 6. 判断header中是否有token，对用户请求进行判断拦截
+        if (StringUtils.isNotBlank(userId) && StringUtils.isNotBlank(userToken)) {
+            String redisToken = redis.get(REDIS_USER_TOKEN + ":" + userId);
+            if (redisToken.equals(userToken)) {
+                // 匹配则放行
+                return chain.filter(exchange);
+            }
+        }
+
+        // 默认不放行
+        return RenderErrorUtils.display(exchange, ResponseStatusEnum.UN_LOGIN);
     }
 
     // 过滤器的顺序，数字越小则优先级越大
