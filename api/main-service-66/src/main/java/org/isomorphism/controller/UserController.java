7ad505d26ce3ac6df7def919721ab6b5a58e125c
Isomorphism <jw6699@nyu.edu>
2025-01-30 20:54:13 -0500

    added method to put user token to return VO after modifying user info

--- api/main-service-66/src/main/java/org/isomorphism/controller/UserController.java
+++ api/main-service-66/src/main/java/org/isomorphism/controller/UserController.java
@@ -1,25 +1,56 @@
 package org.isomorphism.controller;
 
 import jakarta.annotation.Resource;
+import org.isomorphism.base.BaseInfoProperties;
 import org.isomorphism.grace.result.GraceJSONResult;
+import org.isomorphism.pojo.Users;
 import org.isomorphism.pojo.bo.ModifyUserBO;
+import org.isomorphism.pojo.vo.UsersVO;
 import org.isomorphism.service.UsersService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("userinfo")
-public class UserController {
+public class UserController extends BaseInfoProperties {
 
     @Resource
     private UsersService userService;
 
     @PostMapping("modify")
     public GraceJSONResult modify(@RequestBody ModifyUserBO userBO) {
+
+        // 修改用户信息
         userService.modifyUserInfo(userBO);
-        return GraceJSONResult.ok();
+
+        // 返回最新用户信息
+        UsersVO usersVO = getUserInfo(userBO.getUserId(), true);
+
+        return GraceJSONResult.ok(usersVO);
+    }
+
+    private UsersVO getUserInfo(String userId, boolean needToken) {
+
+        // 查询获得用户的最新信息
+        Users latestUser = userService.getById(userId);
+
+        UsersVO usersVO = new UsersVO();
+        BeanUtils.copyProperties(latestUser, usersVO);
+
+        if (needToken) {
+            String uToken = TOKEN_USER_PREFIX + SYMBOL_DOT + UUID.randomUUID();
+            redis.set(REDIS_USER_TOKEN + ":" + userId, uToken);
+            usersVO.setUserToken(uToken);
+        }
+
+        return usersVO;
+    }
+
+    @PostMapping("get")
+    public GraceJSONResult get(@RequestParam String userId) {
+        return GraceJSONResult.ok(getUserInfo(userId, false));
     }
 
 }
