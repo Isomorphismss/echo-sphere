e00cf77030b491a0271c4104ecab315f18afaac7
Isomorphism <jw6699@nyu.edu>
2025-01-30 19:01:04 -0500

    added a register and login function

--- api/auth-service-88/src/main/java/org/isomorphism/controller/PassportController.java
+++ api/auth-service-88/src/main/java/org/isomorphism/controller/PassportController.java
@@ -125,4 +125,46 @@ public class PassportController extends BaseInfoProperties {
         return GraceJSONResult.ok(usersVO);
     }
 
+    /**
+     * 一键注册登录接口，可以同时提供给用户做登录和注册使用调用
+     * @param registerLoginBO
+     * @param request
+     * @return
+     * @throws Exception
+     */
+    @PostMapping("registerOrLogin")
+    public GraceJSONResult registerOrLogin(@RequestBody @Valid RegisterLoginBO registerLoginBO,
+                                    HttpServletRequest request) throws Exception {
+        String mobile = registerLoginBO.getMobile();
+        String code = registerLoginBO.getSmsCode();
+        String nickname = registerLoginBO.getNickname();
+
+        // 1. 从redis中获得验证码进行校验判断是否匹配
+        String redisCode = redis.get(MOBILE_SMSCODE + ":" + mobile);
+        if (StringUtils.isBlank(redisCode) || !redisCode.equalsIgnoreCase(code)) {
+            return GraceJSONResult.errorCustom(ResponseStatusEnum.SMS_CODE_ERROR);
+        }
+
+        // 2. 根据mobile查询数据库，如果用户存在，则直接登录
+        Users user = usersService.queryMobileIfExist(mobile);
+        if (user == null) {
+            // 2.1 如果查询数据库中用户为空，则表示用户没有注册过，则需要进行用户信息数据的入库
+            user = usersService.createUsers(mobile, nickname);
+        }
+
+        // 3. 用户注册成功后，删除redis中的短信验证码使其失效
+        redis.del(MOBILE_SMSCODE + ":" + mobile);
+
+        // 4. 设置用户分布式会话，保存用户的token令牌，存储到redis中
+        String uToken = TOKEN_USER_PREFIX + SYMBOL_DOT + UUID.randomUUID();
+        redis.set(REDIS_USER_TOKEN + ":" + user.getId(), uToken);  // 设置分布式会话
+
+        // 5. 返回用户数据给前端
+        UsersVO usersVO = new UsersVO();
+        BeanUtils.copyProperties(user, usersVO);
+        usersVO.setUserToken(uToken);
+
+        return GraceJSONResult.ok(usersVO);
+    }
+
 }
