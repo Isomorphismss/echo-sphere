0669f00b7e6f4563a06bf8c9a9f329a352338a56
Isomorphism <jw6699@nyu.edu>
2025-01-30 19:34:30 -0500

    added BO and mappers for modifying user info

---
+++ api/main-service-66/src/main/java/org/isomorphism/mapper/UsersMapper.java
@@ -0,0 +1,16 @@
+package org.isomorphism.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.isomorphism.pojo.Users;
+
+/**
+ * <p>
+ * 用户表 Mapper 接口
+ * </p>
+ *
+ * @author isomorphism
+ * @since 2025-01-29
+ */
+public interface UsersMapper extends BaseMapper<Users> {
+
+}
