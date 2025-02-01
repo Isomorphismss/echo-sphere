520854c79e01007ae3694ad0f46f9633184c5948
Isomorphism <jw6699@nyu.edu>
2025-02-01 14:38:07 -0500

    added an openFeign interface for services

---
+++ api/base-service/src/main/java/org/isomorphism/api/feign/UserInfoMicroServiceFeign.java
@@ -0,0 +1,15 @@
+package org.isomorphism.api.feign;
+
+import org.isomorphism.grace.result.GraceJSONResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "main-service")
+public interface UserInfoMicroServiceFeign {
+
+    @PostMapping("/userinfo/updateFace")
+    public GraceJSONResult updateFace(@RequestParam("userId") String userId,
+                                      @RequestParam("face") String face);
+
+}
