7f652dbeeacebe08d5abf0b6c0375359d5ac9b35
Isomorphism <jw6699@nyu.edu>
2025-02-02 16:13:50 -0500

    added mapper class for friend_request table

---
+++ api/main-service-66/src/main/java/org/isomorphism/mapper/FriendRequestMapper.java
@@ -0,0 +1,16 @@
+package org.isomorphism.mapper;
+
+import org.isomorphism.pojo.FriendRequest;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 好友请求记录表 Mapper 接口
+ * </p>
+ *
+ * @author isomorphism
+ * @since 2025-01-29
+ */
+public interface FriendRequestMapper extends BaseMapper<FriendRequest> {
+
+}
