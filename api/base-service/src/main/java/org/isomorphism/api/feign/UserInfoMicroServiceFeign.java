4de2e876da0aa399cac3231488de870fca217671
Isomorphism <jw6699@nyu.edu>
2025-02-02 12:35:49 -0500

    registered the controller method to update friend circle to micro service

--- api/base-service/src/main/java/org/isomorphism/api/feign/UserInfoMicroServiceFeign.java
+++ api/base-service/src/main/java/org/isomorphism/api/feign/UserInfoMicroServiceFeign.java
@@ -4,6 +4,7 @@ import org.isomorphism.grace.result.GraceJSONResult;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @FeignClient(value = "main-service")
 public interface UserInfoMicroServiceFeign {
@@ -11,5 +12,10 @@ public interface UserInfoMicroServiceFeign {
     @PostMapping("/userinfo/updateFace")
     public GraceJSONResult updateFace(@RequestParam("userId") String userId,
                                       @RequestParam("face") String face);
+
+    @PostMapping("/userinfo/updateFriendCircleBg")
+    public GraceJSONResult updateFriendCircleBg(
+            @RequestParam("userId") String userId,
+            @RequestParam("friendCircleBg") String friendCircleBg);
 
 }
