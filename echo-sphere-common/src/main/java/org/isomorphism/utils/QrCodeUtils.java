be7f393cd301178de042fa097249421456faeeab
Isomorphism <jw6699@nyu.edu>
2025-02-01 20:07:41 -0500

    modified the QR code util to stop using local storage, using memory instead

--- echo-sphere-common/src/main/java/org/isomorphism/utils/QrCodeUtils.java
+++ echo-sphere-common/src/main/java/org/isomorphism/utils/QrCodeUtils.java
@@ -18,17 +18,12 @@ import java.util.UUID;
  */
 public class QrCodeUtils {
 
-    public static String generateQRCode(String data) {
-        String filePath = "default/pc/path";
-        return QrCodeUtils.generateQRCode(data, filePath);
-    }
-
-    public static String generateQRCode(String data, String filePath) {
-        return QrCodeUtils.generateQRCode(data, 300, 300, filePath);
+    public static BufferedImage generateQRCodeImage(String data) {
+        return QrCodeUtils.generateQRCodeImage(data, 300, 300);
     }
 
     // 生成二维码
-    public static String generateQRCode(String data, int width, int height, String filePath) {
+    public static BufferedImage generateQRCodeImage(String data, int width, int height) {
         try {
             Map<EncodeHintType, Object> hints = new HashMap<>();
             hints.put(EncodeHintType.CHARACTER_SET, "UTF-8"); // 设置字符编码
@@ -38,40 +33,30 @@ public class QrCodeUtils {
             MultiFormatWriter writer = new MultiFormatWriter();
             BitMatrix bitMatrix = writer.encode(data, BarcodeFormat.QR_CODE, width, height, hints);
 
-            // 创建BufferedImage对象来表示二维码
+            // 创建 BufferedImage 并绘制二维码
             BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
             for (int x = 0; x < width; x++) {
                 for (int y = 0; y < height; y++) {
                     image.setRGB(x, y, bitMatrix.get(x, y) ? Color.BLACK.getRGB() : Color.WHITE.getRGB());
                 }
             }
-
-            // 将QR码保存到文件
-            File qrCodeFile = new File(filePath);
-            ImageIO.write(image, "png", qrCodeFile);
-            // MultipartFile multipartFile = new MockMultipartFile(
-            //                                     "file",
-            //                                     qrCodeFile.getName(),
-            //                                     null,
-            //                                     new FileInputStream(qrCodeFile));
-
-            System.out.println("二维码已生成并保存到: " + filePath);
-            return filePath;
+            System.out.println("图像已写入到内存");
+            return image;
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
 
-    public static void main(String[] args) {
-        // String data = ""; // 二维码中的数据
-        String data = "Isomorphism 测试数据"; // 二维码中的数据
-        int width = 300; // 二维码的宽度
-        int height = 300; // 二维码的高度
-        String uuid = UUID.randomUUID().toString();
-        String filePath = "default/pc/path" + uuid + ".png"; // 生成的二维码文件的路径
-
-        QrCodeUtils.generateQRCode(data, width, height, filePath);
-    }
+//    public static void main(String[] args) {
+//        // String data = ""; // 二维码中的数据
+//        String data = "Isomorphism 测试数据"; // 二维码中的数据
+//        int width = 300; // 二维码的宽度
+//        int height = 300; // 二维码的高度
+//        String uuid = UUID.randomUUID().toString();
+//        String filePath = "your/local/path" + uuid + ".png"; // 生成的二维码文件的路径
+//
+//        QrCodeUtils.generateQRCode(data, width, height, filePath);
+//    }
 
 }
