7693c728aaf5c708f7c2c77750a2a9eb14964061
Isomorphism <jw6699@nyu.edu>
2025-02-03 10:03:59 -0500

    added a controller method to return the paginated friend request result to frontend

--- api/main-service-66/src/main/java/org/isomorphism/controller/FriendRequestController.java
+++ api/main-service-66/src/main/java/org/isomorphism/controller/FriendRequestController.java
@@ -1,20 +1,20 @@
 package org.isomorphism.controller;
 
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.isomorphism.base.BaseInfoProperties;
 import org.isomorphism.grace.result.GraceJSONResult;
 import org.isomorphism.pojo.bo.NewFriendRequestBO;
 import org.isomorphism.service.FriendRequestService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.isomorphism.utils.PagedGridResult;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("friendRequest")
 @Slf4j
-public class FriendRequestController {
+public class FriendRequestController extends BaseInfoProperties {
 
     @Resource
     private FriendRequestService friendRequestService;
@@ -23,6 +23,16 @@ public class FriendRequestController {
     public GraceJSONResult add(@RequestBody @Valid NewFriendRequestBO friendRequestBO) {
         friendRequestService.addNewRequest(friendRequestBO);
         return GraceJSONResult.ok();
+    }
+
+    @PostMapping("queryNew")
+    public GraceJSONResult queryNew(HttpServletRequest request,
+                                    @RequestParam(defaultValue = "1") Integer page,
+                                    @RequestParam(defaultValue = "15") Integer pageSize) {
+        String userId = request.getHeader(HEADER_USER_ID);
+
+        PagedGridResult result = friendRequestService.queryNewFriendList(userId, page, pageSize);
+        return GraceJSONResult.ok(result);
     }
 
 }
