5b2595e2d586d8dcbccfb5ed1fa21c83d0747099
Isomorphism <jw6699@nyu.edu>
2025-02-03 15:38:38 -0500

    added a mapper to get info from friendship table

---
+++ api/main-service-66/src/main/java/org/isomorphism/mapper/FriendshipMapper.java
@@ -0,0 +1,16 @@
+package org.isomorphism.mapper;
+
+import org.isomorphism.pojo.Friendship;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 朋友关系表 Mapper 接口
+ * </p>
+ *
+ * @author isomorphism
+ * @since 2025-01-29
+ */
+public interface FriendshipMapper extends BaseMapper<Friendship> {
+
+}
