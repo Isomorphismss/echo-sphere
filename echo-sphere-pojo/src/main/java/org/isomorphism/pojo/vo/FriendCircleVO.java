cf65ed75e9d21fb0cb917fbdebd25ebad3418833
Isomorphism <jw6699@nyu.edu>
2025-02-05 13:47:38 -0500

    added a VO class for frontend to display friend circle posts

---
+++ echo-sphere-pojo/src/main/java/org/isomorphism/pojo/vo/FriendCircleVO.java
@@ -0,0 +1,38 @@
+package org.isomorphism.pojo.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.isomorphism.pojo.Comment;
+import org.isomorphism.pojo.FriendCircleLiked;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class FriendCircleVO implements Serializable {
+
+    private String FriendCircleId;
+
+    private String userId;
+
+    private String userNickname;
+
+    private String userFace;
+
+    private String words;
+
+    private String images;
+
+    private LocalDateTime publishTime;
+
+    private List<FriendCircleLiked> likedFriends = new ArrayList<>();
+    private List<Comment> commentList = new ArrayList<>();
+
+}
