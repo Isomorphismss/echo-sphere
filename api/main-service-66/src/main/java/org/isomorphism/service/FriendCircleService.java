0ca0adfe016eb970769bf5886e5b16a4ace6957e
Isomorphism <jw6699@nyu.edu>
2025-02-05 13:47:18 -0500

    added a service method to display friend circle posts

--- api/main-service-66/src/main/java/org/isomorphism/service/FriendCircleService.java
+++ api/main-service-66/src/main/java/org/isomorphism/service/FriendCircleService.java
@@ -1,6 +1,8 @@
 package org.isomorphism.service;
 
 import org.isomorphism.pojo.bo.FriendCircleBO;
+import org.isomorphism.utils.PagedGridResult;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface FriendCircleService {
 
@@ -9,5 +11,16 @@ public interface FriendCircleService {
      * @param friendCircleBO
      */
     public void publish(FriendCircleBO friendCircleBO);
+
+    /**
+     * 分页查询朋友圈图文列表
+     * @param userId
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    public PagedGridResult queryList(String userId,
+                                     Integer page,
+                                     Integer pageSize);
 
 }
--- api/main-service-66/src/main/java/org/isomorphism/service/impl/FriendCircleServiceImpl.java
+++ api/main-service-66/src/main/java/org/isomorphism/service/impl/FriendCircleServiceImpl.java
@@ -1,20 +1,30 @@
 package org.isomorphism.service.impl;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import jakarta.annotation.Resource;
 import org.isomorphism.base.BaseInfoProperties;
 import org.isomorphism.mapper.FriendCircleMapper;
+import org.isomorphism.mapper.FriendCircleMapperCustom;
 import org.isomorphism.pojo.FriendCircle;
 import org.isomorphism.pojo.bo.FriendCircleBO;
+import org.isomorphism.pojo.vo.FriendCircleVO;
 import org.isomorphism.service.FriendCircleService;
+import org.isomorphism.utils.PagedGridResult;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class FriendCircleServiceImpl extends BaseInfoProperties implements FriendCircleService {
 
     @Resource
     private FriendCircleMapper friendCircleMapper;
+
+    @Resource
+    private FriendCircleMapperCustom friendCircleMapperCustom;
 
     @Transactional
     @Override
@@ -26,4 +36,17 @@ public class FriendCircleServiceImpl extends BaseInfoProperties implements Frien
         friendCircleMapper.insert(pendingFriendCircle);
     }
 
+    @Override
+    public PagedGridResult queryList(String userId,
+                                     Integer page,
+                                     Integer pageSize) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("userId", userId);
+
+        // 设置分页参数
+        Page<FriendCircleVO> pageInfo = new Page<>(page, pageSize);
+        friendCircleMapperCustom.queryFriendCircleList(pageInfo, map);
+
+        return setterPagedGridPlus(pageInfo);
+    }
 }
