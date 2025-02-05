2b7d1ec7893d484e20527b3587ff92c6647bd8a2
Isomorphism <jw6699@nyu.edu>
2025-02-04 19:07:43 -0500

    added a controller method to update friend remark

--- api/main-service-66/src/main/java/org/isomorphism/controller/FriendshipController.java
+++ api/main-service-66/src/main/java/org/isomorphism/controller/FriendshipController.java
@@ -3,6 +3,7 @@ package org.isomorphism.controller;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.isomorphism.base.BaseInfoProperties;
 import org.isomorphism.grace.result.GraceJSONResult;
 import org.isomorphism.pojo.Friendship;
@@ -22,7 +23,7 @@ public class FriendshipController extends BaseInfoProperties {
     @PostMapping("getFriendship")
     public GraceJSONResult getFriendship(String friendId, HttpServletRequest request) {
         String myId = request.getHeader(HEADER_USER_ID);
-        Friendship friendship = friendshipService.getFriendship(friendId, myId);
+        Friendship friendship = friendshipService.getFriendship(myId, friendId);
         return GraceJSONResult.ok(friendship);
     }
 
@@ -30,6 +31,19 @@ public class FriendshipController extends BaseInfoProperties {
     public GraceJSONResult queryMyFriends(HttpServletRequest request) {
         String myId = request.getHeader(HEADER_USER_ID);
         return GraceJSONResult.ok(friendshipService.queryMyFriends(myId));
+    }
+
+    @PostMapping("updateFriendRemark")
+    public GraceJSONResult updateFriendRemark(HttpServletRequest request,
+                                              String friendId,
+                                              String friendRemark) {
+        if (StringUtils.isBlank(friendId) || StringUtils.isBlank(friendRemark)) {
+            return GraceJSONResult.error();
+        }
+
+        String myId = request.getHeader(HEADER_USER_ID);
+        friendshipService.updateFriendRemark(myId, friendId, friendRemark);
+        return GraceJSONResult.ok();
     }
 
 }
