66d3cc7b62ed48372c80873c0d1ef7fc07584c90
Isomorphism <jw6699@nyu.edu>
2025-02-06 13:05:32 -0500

    created a BO class for frontend comment related requests

---
+++ echo-sphere-pojo/src/main/java/org/isomorphism/pojo/bo/CommentBO.java
@@ -0,0 +1,24 @@
+package org.isomorphism.pojo.bo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentBO {
+
+    private String belongUserId;
+
+    private String friendCircleId;
+
+    private String fatherId;
+
+    private String commentUserId;
+
+    private String commentContent;
+
+}
