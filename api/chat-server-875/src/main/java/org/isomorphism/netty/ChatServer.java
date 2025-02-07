f0008c3cb49a0e9543454668c46509701f59708b
Isomorphism <jw6699@nyu.edu>
2025-02-07 14:09:11 -0500

    added an initializer class for pipeline filters

--- api/chat-server-875/src/main/java/org/isomorphism/netty/ChatServer.java
+++ api/chat-server-875/src/main/java/org/isomorphism/netty/ChatServer.java
@@ -20,10 +20,10 @@ public class ChatServer {
 
         try {
             // 构建Netty服务器
-            ServerBootstrap server = new ServerBootstrap();    // 服务的启动类
+            ServerBootstrap server = new ServerBootstrap();      // 服务的启动类
             server.group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class)     // 设置Nio的双向通道
-                    .childHandler(null);                       // 设置处理器，用于处理workerGroup
+                    .channel(NioServerSocketChannel.class)       // 设置Nio的双向通道
+                    .childHandler(new HttpServerInitializer());  // 设置处理器，用于处理workerGroup
 
             // 启动server，并且绑定端口号875，同时启动方式为“同步”
             ChannelFuture channelFuture = server.bind(875).sync();
