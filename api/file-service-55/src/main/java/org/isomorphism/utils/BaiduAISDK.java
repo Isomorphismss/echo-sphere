5adb825454d6e37f9226c81cda5767e2078e622d
Isomorphism <jw6699@nyu.edu>
2025-02-09 14:59:01 -0500

    fixed the static variable injection issue

--- api/file-service-55/src/main/java/org/isomorphism/utils/BaiduAISDK.java
+++ api/file-service-55/src/main/java/org/isomorphism/utils/BaiduAISDK.java
@@ -1,16 +1,28 @@
 package org.isomorphism.utils;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 
 public class BaiduAISDK {
 
     @Value("${baidu.speech-to-text.app-id}")
-    public static String APP_ID;
+    private String appId;
 
     @Value("${baidu.speech-to-text.api-key}")
-    public static String API_KEY;
+    private String apiKey;
 
     @Value("${baidu.speech-to-text.secret-key}")
+    private String secretKey;
+
+    public static String APP_ID;
+    public static String API_KEY;
     public static String SECRET_KEY;
+
+    @PostConstruct
+    public void init() {
+        APP_ID = appId;
+        API_KEY = apiKey;
+        SECRET_KEY = secretKey;
+    }
 
 }
