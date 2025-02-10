510eb589029d803a03eec92c96ee8b824304b652
Isomorphism <jw6699@nyu.edu>
2025-02-10 15:37:47 -0500

    added a test method to test rabbit mq

--- api/main-service-66/src/main/java/org/isomorphism/controller/HelloController.java
+++ api/main-service-66/src/main/java/org/isomorphism/controller/HelloController.java
@@ -1,5 +1,10 @@
 package org.isomorphism.controller;
 
+import jakarta.annotation.Resource;
+import org.isomorphism.pojo.netty.ChatMsg;
+import org.isomorphism.rabbitmq.RabbitMQTestConfig;
+import org.isomorphism.utils.JsonUtils;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +18,25 @@ public class HelloController {
     @GetMapping("hello")
     public Object hello() {
         return "Hello World";
+    }
+
+    @Resource
+    private RabbitTemplate rabbitTemplate;
+
+    @GetMapping("mq")
+    public Object ok() {
+
+        ChatMsg chatMsg = new ChatMsg();
+        chatMsg.setMsg("hello world");
+        String msg = JsonUtils.objectToJson(chatMsg);
+
+        rabbitTemplate.convertAndSend(
+                RabbitMQTestConfig.TEST_EXCHANGE,
+                "echo.sphere.test.send",
+                msg
+        );
+
+        return "ok";
     }
 
 }
