f6bcc6a74e099fcec39482f3e24c43c0dfa4f5cb
Isomorphism <jw6699@nyu.edu>
2025-02-11 14:19:41 -0500

    added a mapper for chat message related operations

---
+++ api/main-service-66/src/main/java/org/isomorphism/mapper/ChatMessageMapper.java
@@ -0,0 +1,16 @@
+package org.isomorphism.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.isomorphism.pojo.ChatMessage;
+
+/**
+ * <p>
+ * 聊天信息存储表 Mapper 接口
+ * </p>
+ *
+ * @author isomorphism
+ * @since 2025-01-29
+ */
+public interface ChatMessageMapper extends BaseMapper<ChatMessage> {
+
+}
