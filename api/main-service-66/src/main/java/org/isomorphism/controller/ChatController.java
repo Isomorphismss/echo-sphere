f7521b064da1598456bbaa49b2bf2097f04264e5
Isomorphism <jw6699@nyu.edu>
2025-02-11 20:25:04 -0500

    added a function to clear unread counts when user go in single chats

--- api/main-service-66/src/main/java/org/isomorphism/controller/ChatController.java
+++ api/main-service-66/src/main/java/org/isomorphism/controller/ChatController.java
@@ -15,11 +15,15 @@ public class ChatController extends BaseInfoProperties {
     // 127.0.0.1:77/a/hello
 
     @PostMapping("getMyUnReadCounts")
-    public GraceJSONResult ok(String myId) {
-
+    public GraceJSONResult getMyUnReadCounts(String myId) {
         Map map = redis.hgetall(CHAT_MSG_LIST + ":" + myId);
-
         return GraceJSONResult.ok(map);
+    }
+
+    @PostMapping("clearMyUnReadCounts")
+    public GraceJSONResult clearMyUnReadCounts(String myId, String oppositeId) {
+        redis.setHashValue(CHAT_MSG_LIST + ":" + myId, oppositeId, "0");
+        return GraceJSONResult.ok();
     }
 
 }
