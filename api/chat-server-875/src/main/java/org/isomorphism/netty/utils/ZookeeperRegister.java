2810a9bd599dc8175e1f59fe9b60898d3a4eff42
Isomorphism <jw6699@nyu.edu>
2025-02-13 16:49:02 -0500

    added a function to create ephemeral node as server nodes in zookeeper

--- api/chat-server-875/src/main/java/org/isomorphism/netty/utils/ZookeeperRegister.java
+++ api/chat-server-875/src/main/java/org/isomorphism/netty/utils/ZookeeperRegister.java
@@ -3,6 +3,8 @@ package org.isomorphism.netty.utils;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.data.Stat;
+import org.isomorphism.pojo.netty.NettyServerNode;
+import org.isomorphism.utils.JsonUtils;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -24,6 +26,16 @@ public class ZookeeperRegister {
         } else {
             System.out.println(stat);
         }
+
+        // 创建对应的临时节点，值可以放在线人数，默认为初始化的0
+        NettyServerNode serverNode = new NettyServerNode();
+        serverNode.setIp(ip);
+        serverNode.setPort(port);
+        String nodeJson = JsonUtils.objectToJson(serverNode);
+
+        zkClient.create()
+                .withMode(CreateMode.EPHEMERAL_SEQUENTIAL)
+                .forPath(path + "/im-", nodeJson.getBytes());
 
     }
 
