7ab0cd4a970bc70f001af5d7dc58fb173a90adea
Isomorphism <jw6699@nyu.edu>
2025-02-13 16:35:21 -0500

    added a util class to register netty server to zookeeper

---
+++ api/chat-server-875/src/main/java/org/isomorphism/netty/utils/ZookeeperRegister.java
@@ -0,0 +1,37 @@
+package org.isomorphism.netty.utils;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.data.Stat;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class ZookeeperRegister {
+
+    public static void registerNettyServer(String nodeName,
+                                           String ip,
+                                           Integer port) throws Exception {
+
+        CuratorFramework zkClient = CuratorConfig.getClient();
+        String path = "/" + nodeName;
+        Stat stat = zkClient.checkExists().forPath(path);
+        if (stat == null) {
+            zkClient.create()
+                    .creatingParentsIfNeeded()
+                    .withMode(CreateMode.EPHEMERAL_SEQUENTIAL)
+                    .forPath(path);
+        } else {
+            System.out.println(stat);
+        }
+
+    }
+
+    public static String getLocalIp() throws Exception {
+        InetAddress address = InetAddress.getLocalHost();
+        String ip = address.getHostAddress();
+        System.out.println("本机IP地址：" + ip);
+        return ip;
+    }
+
+}
