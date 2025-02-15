b311cb8cf3b0c18e8a64e806f948ed1af94ccb89
Isomorphism <jw6699@nyu.edu>
2025-02-15 15:01:28 -0500

    added a util method to consume message by fanout mode

--- api/chat-server-875/src/main/java/org/isomorphism/netty/mq/RabbitMQConnectUtils.java
+++ api/chat-server-875/src/main/java/org/isomorphism/netty/mq/RabbitMQConnectUtils.java
@@ -3,6 +3,7 @@ package org.isomorphism.netty.mq;
 import com.rabbitmq.client.*;
 import io.github.cdimascio.dotenv.Dotenv;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -92,6 +93,59 @@ public class RabbitMQConnectUtils {
 
     public void setConnection(Connection connection) throws Exception {
         getAndSetConnection(false, connection);
+    }
+
+    public void listen(String exchangeName, String queueName) throws Exception {
+
+        Connection connection = getConnection();
+        Channel channel = connection.createChannel();
+
+        // 定义交换机 FANOUT 发布订阅模式（广播模式）
+        channel.exchangeDeclare(exchangeName,
+                BuiltinExchangeType.FANOUT,
+                true,
+                false,
+                null);
+
+        // 定义队列
+        channel.queueDeclare(queueName,
+                true,
+                false,
+                false,
+                null);
+
+        // 把队列绑定到交换机
+        channel.queueBind(queueName, exchangeName, "");
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            /**
+             * 重写消息配送（交付）方法
+             * @param consumerTag 消息的标签（标识）
+             * @param envelope 信封（一些消息，比如交换机路由等信息）
+             * @param properties 配置信息和内容
+             * @param body 收到的消息数据内容
+             * @throws IOException
+             */
+            @Override
+            public void handleDelivery(String consumerTag,
+                                       Envelope envelope,
+                                       AMQP.BasicProperties properties,
+                                       byte[] body) throws IOException {
+                String exchange = envelope.getExchange();
+                if (exchange.equalsIgnoreCase(exchangeName)) {
+                    String msg = new String(body);
+                    System.out.println(msg);
+                }
+            }
+        };
+
+        /**
+         * queue: 监听的队列名称
+         * autoAck：是否自动确认，true：告知mq消费者已经消费的确认通知
+         * callback：回调函数，处理监听到的消息。
+         */
+        channel.basicConsume(queueName, true, consumer);
+
     }
 
     private synchronized Connection getAndSetConnection(boolean isGet, Connection connection) throws Exception {
