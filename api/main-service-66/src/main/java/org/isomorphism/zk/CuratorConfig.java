6744ecb693f6b3888840da0c35690136ca198f49
Isomorphism <jw6699@nyu.edu>
2025-02-15 13:58:37 -0500

    added a watcher in zookeeper to delete remaining port by already closed server

--- api/main-service-66/src/main/java/org/isomorphism/zk/CuratorConfig.java
+++ api/main-service-66/src/main/java/org/isomorphism/zk/CuratorConfig.java
@@ -5,7 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.cache.CuratorCache;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.isomorphism.pojo.netty.NettyServerNode;
+import org.isomorphism.utils.JsonUtils;
+import org.isomorphism.utils.RedisOperator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -22,6 +27,8 @@ public class CuratorConfig {
     private Integer sleepMsBetweenRetry;     // 每次重试的间隔时间
     private Integer maxRetries;              // 最大重试次数
     private String namespace;                // 命名空间（root根节点名称）
+
+    public static final String PATH = "/server-list";
 
     @Bean("curatorClient")
     public CuratorFramework curatorClient() {
@@ -43,7 +50,71 @@ public class CuratorConfig {
 //            throw new RuntimeException(e);
 //        }
 
+        // 注册监听watcher的事件
+        addWatcher(PATH, client);
+
         return client;
+    }
+
+    @Autowired
+    private RedisOperator redis;
+
+    /**
+     * 注册节点的事件监听
+     * @param path
+     * @param client
+     */
+    public void addWatcher(String path, CuratorFramework client) {
+
+        CuratorCache curatorCache = CuratorCache.build(client, path);
+        curatorCache.listenable().addListener((type, oldData, data) -> {
+
+            // type: 当前监听到的事件类型
+            // oldData: 节点更新前的数据、状态
+            // data: 节点更新后的数据和状态
+
+            System.out.println(type);
+
+//            if (oldData != null) {
+//                System.out.println("old path = " + oldData.getPath());
+//                System.out.println("old value = " + String.valueOf(oldData.getData()));
+//            }
+//
+//            if (data != null) {
+//                System.out.println("new path = " + data.getPath());
+//                System.out.println("new value = " + String.valueOf(data.getData()));
+//            }
+
+            switch (type.name()) {
+                case "NODE_CREATED":
+                    log.info("(子)节点创建");
+                    break;
+                case "NODE_CHANGED":
+                    log.info("(子)节点(数据)变更");
+                    break;
+                case "NODE_DELETED":
+                    log.info("(子)节点删除");
+
+                    NettyServerNode oldNode = JsonUtils.jsonToPojo(
+                            new String(oldData.getData()),
+                            NettyServerNode.class);
+
+                    System.out.println("old path = " + oldData.getPath());
+                    System.out.println("old value = " + oldNode);
+
+                    String oldPort = oldNode.getPort() + "";
+                    String portKey = "netty_port";
+                    redis.hdel(portKey, oldPort);
+
+                    break;
+                default:
+                    log.info("default...");
+                    break;
+            }
+
+        });
+
+        curatorCache.start();
     }
 
 }
